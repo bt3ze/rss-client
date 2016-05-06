d7585b7833dc2d0e6e1b697565113898c99bd95f
Ben Terner <bt3ze@virginia.edu>
2016-05-05 17:40:27 -0700

    docker debug

--- newssource.py
+++ newssource.py
@@ -75,7 +75,7 @@ class rssfeed:
             else:
                 self.fetched_urls.append(news_item.url)
                 news_item.retrieve()
-            print ("update keywords: ",news_item.keywords)
+            #print ("update keywords: ",news_item.keywords)
 
         new_list = self.get_news()        
         return map(fetch_fn,new_list)
@@ -90,7 +90,7 @@ class rssfeed:
                     print ("retrieve failed: ",news_item.url)
                 else:
                     self.fetched_urls.append(news_item.url)
-                    #print ("update keywords: ",news_item.keywords)
+                    print ("update keywords: ",news_item.keywords)
                     return { "url": news_item.url, "keywords": news_item.keywords, "item":news_item }
 
         #print("Fast Update!")
