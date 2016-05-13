7fa470902b283fd16f8ca360ad7774710c5a3b5a
Ben Terner <bt3ze@virginia.edu>
2016-05-12 19:03:07 -0700

    do not want connection reset by peer for requesting too fast

--- feedreader.py
+++ feedreader.py
@@ -191,7 +191,8 @@ class feedreader:
         
     def fast_update_and_dispatch(self):
         def update_and_dispatch(feed):
-            newsitems = feed.fast_update()
+            #newsitems = feed.fast_update()
+            newsitems = feed.update()
             print (newsitems)
             return threaded_map(self.dispatch_fn,newsitems)
         
